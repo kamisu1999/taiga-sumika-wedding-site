8573f4d996319709cd44b5c4997d83872acfd6c1
kamisu1999 <kamisu1999@gmail.com>
2025-08-13 11:24:44 +0900

    Add files via upload

---
+++ wedding-site/wedding-site-github/common.js
@@ -0,0 +1,2 @@
+fetch('header.html').then(res => res.text()).then(html => document.getElementById('site-header').innerHTML = html);
+fetch('footer.html').then(res => res.text()).then(html => document.getElementById('site-footer').innerHTML = html);
