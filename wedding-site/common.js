fbb6c5d8a03146857aa93d0318d46abe5006d4c8
kamisu1999 <kamisu1999@gmail.com>
2025-08-13 11:25:50 +0900

    Delete wedding-site directory

--- wedding-site/common.js
+++
@@ -1,7 +0,0 @@
-fetch('header.html')
-  .then(res => res.text())
-  .then(html => document.getElementById('site-header').innerHTML = html);
-
-fetch('footer.html')
-  .then(res => res.text())
-  .then(html => document.getElementById('site-footer').innerHTML = html);
